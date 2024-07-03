7885015c5a102c1a0e9203a5aa0bfde9386f7ffc
Kemish Hendershot <kemish@Kemishs-MacBook-Pro.local>
2024-07-03 17:04:33 -0600

    Added info back into footer
    Signed-off-by: Kemish Hendershot <kemish@Kemishs-MacBook-Pro.local>

--- pages/home.php
+++ pages/home.php
@@ -1,16 +1,12 @@
 <!-- Main Content -->
- <div class="container">
-    <div class="row">&nbsp;</div>
-    <div class="row">&nbsp;</div>
-    <div class="row">&nbsp;</div>
-    <div class="row">&nbsp;</div>
-    <div class="row">&nbsp;</div>
-    <div class="row">&nbsp;</div>
- </div>
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-lg-12 my-4">
+        <div class="col-md-12 col-lg-12 my-5">
             <h1 class="text-center">Coming Soon!</h1>
+            <p class="text-center">We have some exciting projects coming to Utah and Salt Lake Counties. <br>Check back soon for more info!</p>
         </div>
+    </div>
+    <div class="row blank-space">
+        
     </div>
 </div>
---
+++ template/footer.php
@@ -0,0 +1,19 @@
+   
+        <!-- Footer -->
+        <footer class="bg-dark py-4">
+            <div class="container">
+                <div class="row my-4">
+                    <div class="col-md-12">
+                        <h2 class="display-4 text-center">Call us at <span class="text-orange">(385) 323-2290</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <p class="text-center mb-0">&copy;<?php echo date("Y"); ?> Timberline Building Company. &nbsp;All rights reserved.</p>
+                    <p class="text-center mt-0">UT Contractor License 13640252-5501</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
