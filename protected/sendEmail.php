5d45f012fb66baf79d8bcf6fc8ea0a4f91845354
Kemish Hendershot <kemish@Kemishs-MacBook-Pro.local>
2025-04-17 14:27:29 -0600

    Fixed email sending error
    Signed-off-by: Kemish Hendershot <kemish@Kemishs-MacBook-Pro.local>

--- protected/sendEmail.php
+++ protected/sendEmail.php
@@ -11,7 +11,7 @@ require 'vendor/PHPMailer/src/SMTP.php';
 $mail = new PHPMailer();
 
 // Configure an SMTP
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 //$mail->SMTPDebug = 3; //Alternative to above constant
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
@@ -23,4 +23,4 @@ $mail->Port = 587;
 
 // Recipients
 $mail->addAddress('kemish.hendershot@gmail.com', 'Website Admin');
-$mail->addReplyTo('info@example.com');
+$mail->addReplyTo('info@tbcutah.com');
